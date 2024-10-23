48da99d0d7f3d7516ea2577c843a6fa10686a5fa
Vio1etever1yst <149240651+Vio1etever1yst@users.noreply.github.com>
2024-10-23 08:14:04 +0800

    Add files via upload

---
+++ OddEven/src/OddEven.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int oddCount = 0;
+        int evenCount = 0;
+
+        while (true) {
+            int number = scanner.nextInt();
+            if (number == -1) break;
+            if (number % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println(oddCount + " " + evenCount);
+        scanner.close();
+    }
+}
